d145d4e8f1746591c15c47660d4c1628db1f2ced
Ana Ćavar <ana.cavar22@gmail.com>
2024-09-24 20:52:57 +0200

    file cleanup

--- DMC/singlerunDMC.c
+++ DMC/singlerunDMC.c
@@ -8,26 +8,18 @@ double dtau;
 
 int main(int argc, char *argv[])
 {
-  // int Nt = 1000, Nw = 500, Nb = 100, NbSkip = 0;
-  // int Nt = 1000, Nw = 500, Nb = 1050, NbSkip = 50;
-  // int Nt = 1000, Nw = 100, Nb = 220, NbSkip = 20;
-  // int Nt = 1000, Nw = 300, Nb = 350, NbSkip = 50;
   int Nt = 1000, Nw = 100, Nb = 250, NbSkip = 50;
-  // int Nt = 1000, Nw = 100, Nb = 100, NbSkip = 0;
+
   char batchScript[256];
   snprintf(batchScript, sizeof(batchScript), "C:\\repos\\2Dtrimer\\DMC\\prerunVMC.bat %d %d %d %d", Nt, Nw, Nb, NbSkip);
-  // system(batchScript);
+  // system(batchScript); // za prethodno pokretanje VMC, kako bi odgovarali početni parametri šetača
 
   double E, sigmaE;
   gamma_var = gamma_initial;
   alpha = alpha_initial;
   s = s_initial;
-  // double gamma_var = 4.8;
-  // double alpha = 4.50;
-  // double s = 0.28;
-  // dtau = 1.0 * pow(10, -3);
   dtau = 1.0 * pow(10, -5);
-  // dtau = 1.0 * pow(10, -5);
+  
   if(argc == 5){
     int Nt = atoi(argv[1]), Nw = atoi(argv[2]), Nb = atoi(argv[3]), NbSkip = atoi(argv[4]);
     DMC(&E, &sigmaE, Nt, Nw, Nb, NbSkip);
